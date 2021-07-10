bbcd80599f618fbb5ed2ed3f3594abd492e33cc2
Taniel Rémi <taniel.remi@gmail.com>
2021-07-10 15:21:16 +0200

    :sparkles: : add logout

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { UntilDestroy } from '@ngneat/until-destroy';
-import { Observable, of } from 'rxjs';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { Observable } from 'rxjs';
 import { first } from 'rxjs/operators';
 import { User } from '../../models/user';
+import { AuthSelector } from '../../store/auth/auth-selector.service';
 import { SettingsDispatcher } from '../../store/settings/settings-dispatcher.service';
 import { SettingsSelector } from '../../store/settings/settings-selector.service';
 
@@ -13,17 +14,14 @@ import { SettingsSelector } from '../../store/settings/settings-selector.service
     styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-    user$: Observable<User>;
+    user$: Observable<User | null>;
 
     constructor(
         private readonly settingsDispatcher: SettingsDispatcher,
         private readonly settingsSelector: SettingsSelector,
+        private readonly authSelector: AuthSelector,
     ) {
-        this.user$ = of({
-            id: '1',
-            email: 'taniel.remi@gmail.com',
-            pseudo: 'Tanou',
-        });
+        this.user$ = this.authSelector.user$.pipe(untilDestroyed(this));
     }
 
     changeNavigationVisibility(): void {
